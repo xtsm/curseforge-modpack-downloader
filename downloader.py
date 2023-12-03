a8d662b9fab74824be7719a4e90ecb2da0bc316a
Stanislau Tsitsianok <tsmx@tut.by>
2023-12-03 14:08:20 +0300

    old api is dead, long live new api

--- downloader.py
+++ downloader.py
@@ -10,25 +10,24 @@ import os
 async def download_file(sess, file_id, output_dir, no_cache):
   pid = file_id["projectID"]
   fid = file_id["fileID"]
-  api_url = f"https://addons-ecs.forgesvc.net/api/v2/addon/{pid}/file/{fid}"
+  api_url = f"https://www.curseforge.com/api/v1/mods/{pid}/files/{fid}"
   async with sess.get(api_url) as req:
     file_info = await req.json()
 
-  download_url = file_info["downloadUrl"]
-  file_name = file_info["fileName"]
-  file_length = file_info["fileLength"]
-  display_name = file_info["displayName"]
+  file_name = file_info["data"]["fileName"]
+  file_length = file_info["data"]["fileLength"]
   file_path = os.path.join(output_dir, file_name)
-  
+
   use_cache = not no_cache and os.path.isfile(file_path) and os.path.getsize(file_path) == file_length
   if not use_cache:
+    download_url = f"https://www.curseforge.com/api/v1/mods/{pid}/files/{fid}/download"
     async with sess.get(download_url) as req:
       file_contents = await req.read()
     assert len(file_contents) == file_length
     with open(file_path, "wb") as f:
       f.write(file_contents)
 
-  return display_name, file_length, use_cache
+  return file_name, file_length, use_cache
 
 
 async def main():
@@ -49,9 +48,11 @@ async def main():
 
   async with aiohttp.ClientSession() as sess:
     coros = [download_file(sess, f, args.output_dir, args.no_cache) for f in files]
-    for coro in asyncio.as_completed(coros):
-      completed += 1
+    # freezes sometimes but if you're desperate, you can use it, might speed things up
+    # for coro in asyncio.as_completed(coros):
+    for coro in coros:
       filename, length, used_cache = await coro
+      completed += 1
       print(f"[{completed}/{len(files)}] {filename} ({length}B{' cached' if used_cache else ''})")
 
 
